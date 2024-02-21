5ffafbd8f0fbd87db74f8d9854960d53b04ca607
m-bartos <m.bartos96@seznam.cz>
2024-02-21 21:14:28 +0100

    Update: PhoneBook class

--- CPP_01/ex01/PhoneBook.cpp
+++ CPP_01/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 14:27:33 by mbartos           #+#    #+#             */
-/*   Updated: 2024/02/21 20:26:56 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/02/21 21:12:14 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,28 +23,42 @@
 	};
 
     void PhoneBook::searchContact() const {
-        std::cout << std::setw(10) << std::right << "Index" << " | "
-                  << std::setw(10) << std::right << "First Name" << " | "
-                  << std::setw(10) << std::right << "Last Name" << " | "
-                  << std::setw(10) << std::right << "Nickname" << std::endl;
+        std::cout << "|" 
+				  << std::setw(10) << std::right << "Index" << "|"
+                  << std::setw(10) << std::right << "First Name" << "|"
+                  << std::setw(10) << std::right << "Last Name" << "|"
+                  << std::setw(10) << std::right << "Nickname" << "|" << std::endl;
         
-        for (size_t i = 0; i < contactCount; ++i) {
-            std::cout << std::setw(10) << std::right << i << " | "
-                      << std::setw(10) << std::right << truncateString(contacts[i].getFirstName()) << " | "
-                      << std::setw(10) << std::right << truncateString(contacts[i].getLastName()) << " | "
-                      << std::setw(10) << std::right << truncateString(contacts[i].getNickname()) << std::endl;
+        for (size_t i = 0; i < contactCount; ++i)
+		{
+            std::cout << "|" 
+					  << std::setw(10) << std::right << i << "|"
+                      << std::setw(10) << std::right << truncateString(contacts[i].getFirstName()) << "|"
+                      << std::setw(10) << std::right << truncateString(contacts[i].getLastName()) << "|"
+                      << std::setw(10) << std::right << truncateString(contacts[i].getNickname()) << "|" << std::endl;
         }
+		size_t index;
+		bool validIndex = false;
+		while (!validIndex)
+		{
+			std::cout << "Enter index of contact to display: " << std::endl;
+			std::cin >> index;
+			if (index >= 0 && index < contactCount)
+				validIndex = true;
+			else
+				std::cout << "Invalid index. Please try again." << std::endl;
+		}
+
+		contacts[index].displayContact();
     }
 
     std::string PhoneBook::truncateString(const std::string& str) const {
-        if (str.length() > 10) {
+        if (str.length() > 10)
             return (str.substr(0, 9) + ".");
-        }
         return (str);
     }
 
 	void PhoneBook::addContact(Contact newContact) {
-		// add if contactCount < 8 otherwise change one to another
 		if (this->contactCount < 8)
 		{
 			this->contacts[this->contactCount] = newContact;
