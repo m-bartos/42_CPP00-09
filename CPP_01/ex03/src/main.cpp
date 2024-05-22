5da5f168e94e97c9f72a708030cc0144de48b41a
m-bartos <m.bartos96@seznam.cz>
2024-05-22 14:28:35 +0200

    Clean: CPP_01/ex03 main.cpp

--- CPP_01/ex03/src/main.cpp
+++ CPP_01/ex03/src/main.cpp
@@ -21,14 +21,12 @@ int main()
 		Weapon club = Weapon("crude spiked club");
 		HumanA bob("Bob", club);
 		bob.attack();
-		// Weapon club2 = Weapon("test club2");
-		// bob.setWeapon(club2);
-		// bob.attack();
 		club.setType("some other type of club");
 		bob.attack();
 	}
 	{
 		Weapon club = Weapon("crude spiked club");
+
 		HumanB jim("Jim");
 		jim.setWeapon(club);
 		jim.attack();
