ed8af53c3b65bfc0ad44b583a7761bf6d563ec7c
m-bartos <m.bartos96@seznam.cz>
2024-05-22 14:46:12 +0200

    Update: CPP_01/ex04 main.cpp

--- CPP_01/ex04/main.cpp
+++ CPP_01/ex04/main.cpp
@@ -20,7 +20,7 @@ void replaceWords(std::ifstream& inputFile, std::ofstream& outputFile, std::stri
 	while (std::getline(inputFile, line)) { // Loop through each line in the input file
 		size_t	pos = line.find(searchWord); // Find the position of the first searchWord in the line
 
-		while (pos != std::string::npos) { // Repeat until all occurrences are replaced in the line
+		while (pos != std::string::npos) { //npos is constant (max value of size_t) and is used to indicate that a search operation did not find a match
 			line.erase(pos, searchWord.length()); // Erase searchWord
 			line.insert(pos, replaceWord); // Insert replaceWord instead
 			pos = line.find(searchWord, pos + replaceWord.length()); // Find the next occurrence of the searchWord in the line
@@ -31,32 +31,33 @@ void replaceWords(std::ifstream& inputFile, std::ofstream& outputFile, std::stri
 
 int main(int argc, char **argv) {
 	if (argc == 4) {
+
+		// Check if searchWord is not empty (!= "")
+		std::string		searchWord = argv[2]; 
+		if (searchWord.empty()) {
+			std::cerr << "ERROR: Word to find cannot be empty!" << std::endl;
+			return (1);
+		}
+
+		// Open input file
 		std::ifstream	inputFile;
 		inputFile.open(argv[1]); // Open the input file
 		if(!inputFile.is_open()) {
 			std::cerr << "ERROR: Failed to open infile." << std::endl;
-			return (1);
+			return (2);
 		}
 
 		// Making new name for outFileName by appending ".replace"
 		std::string	outFileName = std::string(argv[1]) + ".replace";
 		// Has to convert the outFileName to char* because of .open() function - cannot handle strings
 		const char	*outFileNameChar = outFileName.c_str();
-		
+
+		// Open outfile
 		std::ofstream	outputFile;
 		outputFile.open(outFileNameChar); // Create or overwrite the output file
 		if(!outputFile.is_open()) {
 			std::cerr << "ERROR: Failed to open outfile." << std::endl;
 			inputFile.close();
-			return (2);
-		}
-
-		std::string		searchWord = argv[2]; 
-		// Check if searchWord is not empty (!= "")
-		if (searchWord == "") {
-			inputFile.close();
-			outputFile.close();
-			std::cerr << "ERROR: Word to find cannot be empty!" << std::endl;
 			return (3);
 		}
 
