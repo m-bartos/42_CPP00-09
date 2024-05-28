3a0d12e46698ddccf0063906799fe385ff60bb6d
m-bartos <m.bartos96@seznam.cz>
2024-05-28 13:59:05 +0200

    Update CPP_03/ex00: add tests in main.c

--- CPP_03/ex00/main.cpp
+++ CPP_03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 11:19:39 by mbartos           #+#    #+#             */
-/*   Updated: 2024/05/28 11:34:29 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/05/28 13:58:34 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,17 @@ int main () {
 
 	firstClapTrap.attack("Evaluator");
 	firstClapTrap.takeDamage(4);
+	firstClapTrap.beRepaired(2);
+
+	std::cout << "--------------------------------" << std::endl;
+	std::cout << "Stats of ClapTrap " << firstClapTrap.getName() << ":" << std::endl;
+	std::cout << "Hitpoints: " << firstClapTrap.getHitPoints() << std::endl;
+	std::cout << "Energy points: " << firstClapTrap.getEnergyPoints() << std::endl;
+	std::cout << "Attack damage: " << firstClapTrap.getAttackDamage() << std::endl;
+	std::cout << "--------------------------------" << std::endl;
+
+	for (int i = 0; i < 9; i++)
+		firstClapTrap.attack("Evaluator");
 	firstClapTrap.beRepaired(2);
 
 	std::cout << "--------------------------------" << std::endl;
