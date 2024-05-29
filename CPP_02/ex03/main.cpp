8093117ed451ae35d8454c99ad9c58b91f2ad7b9
m-bartos <m.bartos96@seznam.cz>
2024-05-29 14:12:40 +0200

    Update CPP_02/ex03: tests in main.cpp

--- CPP_02/ex03/main.cpp
+++ CPP_02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 09:26:11 by mbartos           #+#    #+#             */
-/*   Updated: 2024/05/29 13:52:53 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/05/29 14:11:42 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ int main( void ) {
 	const Point	a(0.,0.);
 	const Point	b(0.,10.);
 	const Point c(10.,0.);
-	const Point P1(0,0);
+	const Point P1(1,1);
 	const Point P2(5,5);
 	const Point P3(9,10);
 	const Point P4(-5,5);
-	const Point P5(1,1);
+	const Point P5(0,0);
 
 	std::cout << "P1 inside triangle (different deffinitions of triangle):" << std::endl;
 	std::cout << (bsp(a, c, b, P1) ? "in" : "out") << std::endl;
@@ -36,10 +36,10 @@ int main( void ) {
 	std::cout << std::endl;
 
 	std::cout << "P2, P3, P4, P5 inside triangle:" << std::endl;
-	std::cout << "P2 should be in: " << (bsp(a, b, c, P2) ? "in" : "out") << std::endl;
+	std::cout << "P2 is on edge -> should be OUT: " << (bsp(a, b, c, P2) ? "in" : "out") << std::endl;
 	std::cout << "P3 should be out: " << (bsp(a, b, c, P3) ? "in" : "out") << std::endl;
 	std::cout << "P4 should be out: " << (bsp(a, b, c, P4) ? "in" : "out") << std::endl;
-	std::cout << "P5 should be in: " << (bsp(a, b, c, P5) ? "in" : "out") << std::endl;
+	std::cout << "P5 is a vertex -> should be OUT: " << (bsp(a, b, c, P5) ? "in" : "out") << std::endl;
 
 	return (0);
 }
