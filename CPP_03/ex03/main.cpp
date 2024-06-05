da52572575719040cf2578dc85c40ce78ec487d7
m-bartos <m.bartos96@seznam.cz>
2024-06-05 10:19:44 +0200

    Update CPP_03/ex03: main() fnc

--- CPP_03/ex03/main.cpp
+++ CPP_03/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 11:19:39 by mbartos           #+#    #+#             */
-/*   Updated: 2024/06/05 10:13:09 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/06/05 10:17:52 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,36 +47,36 @@ int main () {
 	// std::cout << "Attack damage: " << firstClapTrap.getAttackDamage() << std::endl;
 	// std::cout << "--------------------------------" << std::endl;
 
-	std::cout << "Creating firstScavTrap: " << std::endl;
-	ScavTrap firstScavTrap("ScavTrap1");
+	// std::cout << "Creating firstScavTrap: " << std::endl;
+	// ScavTrap firstScavTrap("ScavTrap1");
 	
-	std::cout << std::endl;
-	std::cout << "Copying firstScavTrap to new secondScavTrap: " << std::endl;
-	ScavTrap secondScavTrap(firstScavTrap);
+	// std::cout << std::endl;
+	// std::cout << "Copying firstScavTrap to new secondScavTrap: " << std::endl;
+	// ScavTrap secondScavTrap(firstScavTrap);
 	
-	std::cout << std::endl;
-	std::cout << "Creating thirdScavTrap without any name: " << std::endl;
-	ScavTrap thirdScavTrap;
+	// std::cout << std::endl;
+	// std::cout << "Creating thirdScavTrap without any name: " << std::endl;
+	// ScavTrap thirdScavTrap;
 
-	std::cout << std::endl;
-	std::cout << "Copying attributes from secondScavTrap to thirdScavTrap: " << std::endl;
-	thirdScavTrap = secondScavTrap;
+	// std::cout << std::endl;
+	// std::cout << "Copying attributes from secondScavTrap to thirdScavTrap: " << std::endl;
+	// thirdScavTrap = secondScavTrap;
 
-	std::cout << std::endl;
+	// std::cout << std::endl;
 
-	firstScavTrap.attack("NOTHING_1");
-	secondScavTrap.attack("NOTHING_2");
-	thirdScavTrap.attack("NOTHING_3");
+	// firstScavTrap.attack("NOTHING_1");
+	// secondScavTrap.attack("NOTHING_2");
+	// thirdScavTrap.attack("NOTHING_3");
 
-	std::cout << std::endl;
-	std::cout << "Printing name of secondScavTrap: " << secondScavTrap.getName() << std::endl;
-	std::cout << std::endl;
+	// std::cout << std::endl;
+	// std::cout << "Printing name of secondScavTrap: " << secondScavTrap.getName() << std::endl;
+	// std::cout << std::endl;
 
-	FragTrap frag1("Frag1");
+	// FragTrap frag1("Frag1");
 
-	frag1.highFiveGuys();
-	frag1.attack("NOOONE");
-	std::cout << std::endl;
+	// frag1.highFiveGuys();
+	// frag1.attack("NOOONE");
+	// std::cout << std::endl;
 	DiamondTrap diamond1("Diamond1");
 	std::cout << std::endl;
 	DiamondTrap diamond2(diamond1);
