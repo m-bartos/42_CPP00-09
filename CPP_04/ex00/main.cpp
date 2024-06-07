6f6b5c7099a1144c9e58db28a9aacac029eba550
m-bartos <m.bartos96@seznam.cz>
2024-06-07 11:26:03 +0200

    Update CPP_04/ex00: main.cpp

--- CPP_04/ex00/main.cpp
+++ CPP_04/ex00/main.cpp
@@ -6,13 +6,16 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 12:54:31 by mbartos           #+#    #+#             */
-/*   Updated: 2024/06/07 10:18:01 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/06/07 11:23:08 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
 // int main() {
 // 	const Animal* meta = new Animal();
@@ -28,19 +31,32 @@
 // }
 
 int	main() {
-	const Animal	newAnimal;
-	const Dog		Ralf;
-	const Cat		Mice;
+	const Animal*	newAnimal = new Animal();
+	const Animal*	Ralf = new Dog();
+	const Animal*	Mice = new Cat();
 
 	std::cout << std::endl;
-	std::cout << "Type is \"" << newAnimal.getType() << "\"" << std::endl;
-	newAnimal.makeSound();
+	std::cout << "Type is \"" << newAnimal->getType() << "\"" << std::endl;
+	newAnimal->makeSound();
 	std::cout << std::endl;
-	std::cout << "Type is \"" << Ralf.getType() << "\"" << std::endl;
-	Ralf.makeSound();
+	std::cout << "Type is \"" << Ralf->getType() << "\"" << std::endl;
+	Ralf->makeSound();
 	std::cout << std::endl;
-	std::cout << "Type is \"" << Mice.getType() << "\"" << std::endl;
-	Mice.makeSound();
+	std::cout << "Type is \"" << Mice->getType() << "\"" << std::endl;
+	Mice->makeSound();
 	std::cout << std::endl;
+
+	delete newAnimal;
+	delete Ralf;
+	delete Mice;
+	std::cout << std::endl;
+
+	const WrongAnimal* wrongMice = new WrongCat();
+	std::cout << std::endl;
+	std::cout << "Type is \"" << wrongMice->getType() << "\"" << std::endl;
+	wrongMice->makeSound(); // will output sound of WrongAnimal not WrongCat
+	std::cout << std::endl;
+
+	delete wrongMice;
 	return (0);
 }
