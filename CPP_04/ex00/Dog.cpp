c871b93ff29e8a7ba54026ac67e174fae8012f47
m-bartos <m.bartos96@seznam.cz>
2024-06-07 13:02:27 +0200

    Update CPP_04/ex00: add comment to Dog.cpp

--- CPP_04/ex00/Dog.cpp
+++ CPP_04/ex00/Dog.cpp
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 14:24:36 by mbartos           #+#    #+#             */
-/*   Updated: 2024/06/07 11:08:20 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/06/07 12:41:47 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,9 @@ Dog::~Dog() {
 	std::cout << "Dog destructed!" << std::endl;
 }
 
+// A function that is virtual in a base class is implicitly virtual in its
+// derived classes. When a derived class overrides a virtual, the parame-
+// ters in the base and derived classes must match exactly.
 void Dog::makeSound() const {
 	std::cout << "Barg, barg!" << std::endl;
 }
