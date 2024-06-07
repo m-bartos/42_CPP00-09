3995af3bba35b66802fc5f01307fbe902d611bae
m-bartos <m.bartos96@seznam.cz>
2024-06-07 11:26:39 +0200

    Add CPP_04/ex00: WrongCat class

---
+++ CPP_04/ex00/WrongCat.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongCat.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 11:03:28 by mbartos           #+#    #+#             */
+/*   Updated: 2024/06/07 11:10:03 by mbartos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "WrongCat.hpp"
+
+WrongCat::WrongCat() : WrongAnimal() {
+	this->type = "WrongCat";
+	std::cout << "WrongCat constructed!" << std::endl;
+}
+
+WrongCat::WrongCat(const WrongCat &refObj) : WrongAnimal(refObj) {
+	std::cout << "WrongCat constructed!" << std::endl;
+}
+
+WrongCat& WrongCat::operator=(const WrongCat &refObj) {
+	if (this != &refObj)
+	{
+		this->type = refObj.type;
+	}
+	return (*this);
+}
+
+WrongCat::~WrongCat() {
+	std::cout << "WrongCat destructed!" << std::endl;
+}
+
+void WrongCat::makeSound() const {
+	std::cout << "Wrong meow, wrong meow!" << std::endl;
+}
