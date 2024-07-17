9f929990b7298f0956b3fb2c6febf99a687e47c0
m-bartos <m.bartos96@seznam.cz>
2024-07-17 13:37:27 +0200

    Update CPP_05/ex00: main.cpp

--- CPP_05/ex00/main.cpp
+++ CPP_05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 11:30:57 by mbartos           #+#    #+#             */
-/*   Updated: 2024/07/12 14:07:50 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/07/17 13:37:01 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,27 +30,27 @@ int	main()
 			std::cout << "Trying to decrement '" << neemand.getName() << "'" << std::endl;
 			neemand.decrementGrade();
 		}
-		catch (const Bureaucrat::GradeTooLowException& e)
+		catch (std::exception& e)
 		{
-			std::cerr << "Exception: " << e.what() << std::endl;
+			std::cerr << "Error: " << e.what() << std::endl;
 		}
 		try
 		{
 			std::cout << "Trying to decrement '" << plebAli.getName() << "'" << std::endl;
 			plebAli.decrementGrade();
 		}
-		catch (const Bureaucrat::GradeTooLowException& e)
+		catch (std::exception& e)
 		{
-			std::cerr << "Exception: " << e.what() << std::endl;
+			std::cerr << "Error: " << e.what() << std::endl;
 		}
 		try
 		{
 			std::cout << "Trying to increment '" << bobTheBoss.getName() << "'" << std::endl;
 			bobTheBoss.incrementGrade();
 		}
-		catch (const Bureaucrat::GradeTooHighException& e)
+		catch (std::exception& e)
 		{
-			std::cerr << "Exception: " << e.what() << std::endl;
+			std::cerr << "Error: " << e.what() << std::endl;
 		}
 	}
 	{
@@ -60,13 +60,9 @@ int	main()
 			std::cout << "Trying to construct bureaucrat with grade 0" << std::endl;
 			Bureaucrat highGrade = Bureaucrat("Boss", 0);
 		}
-		catch (const Bureaucrat::GradeTooLowException& e)
+		catch (std::exception& e)
 		{
-			std::cerr << "Exception: " << e.what() << std::endl;
-		}
-		catch (const Bureaucrat::GradeTooHighException& e)
-		{
-			std::cerr << "Exception: " << e.what() << std::endl;
+			std::cerr << "Error: " << e.what() << std::endl;
 		}
 		std::cout << "------------------" << std::endl;
 	}
@@ -77,32 +73,12 @@ int	main()
 			std::cout << "Trying to construct bureaucrat with grade 151" << std::endl;
 			Bureaucrat highGrade = Bureaucrat("Boss", 151);
 		}
-		catch (const Bureaucrat::GradeTooLowException& e)
+		catch (std::exception& e)
 		{
-			std::cerr << "Exception: " << e.what() << std::endl;
-		}
-		catch (const Bureaucrat::GradeTooHighException& e)
-		{
-			std::cerr << "Exception: " << e.what() << std::endl;
+			std::cerr << "Error: " << e.what() << std::endl;
 		}
 		std::cout << "------------------" << std::endl;
 	}
-	// OR
-	{
-		std::cout << "------------------" << std::endl;
-		try
-		{
-			std::cout << "Trying to construct bureaucrat with grade 151" << std::endl;
-			Bureaucrat highGrade = Bureaucrat("Boss", 151);
-		}
-		catch (const std::exception& e)
-		{
-			std::cerr << "Exception: " << e.what() << std::endl;
-		}
-		std::cout << "------------------" << std::endl;
-	}
-	// {
-	// 	Bureaucrat highGrade = Bureaucrat("Boss", 0);
-	// }
+
 	return (0);
 }
